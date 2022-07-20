1b1f16fda1eb996355c9c47b93aa3095324fff64
Chung <chung19941024@gmail.com>
2022-07-20 20:39:47 +0800

    name form validation

--- src/app/pages/name-form/name-form.component.ts
+++ src/app/pages/name-form/name-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { SocketService } from 'src/app/services/socket.service';
 
@@ -10,10 +15,23 @@ import { SocketService } from 'src/app/services/socket.service';
 })
 export class NameFormComponent implements OnInit {
   nameForm: FormGroup = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.maxLength(8),
+        Validators.minLength(2),
+        Validators.pattern('^[a-zA-Z0-9]+$'),
+        this.validateNameAllow,
+      ])
+    ),
   });
 
   constructor(private router: Router, private socketService: SocketService) {}
+
+  validateNameAllow(control: AbstractControl) {
+    return control.value === 'officer' ? { notAllow: true } : null;
+  }
 
   ngOnInit(): void {}
 
