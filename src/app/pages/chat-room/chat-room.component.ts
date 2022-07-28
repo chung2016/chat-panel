81d59e7cafd4f2c2fec8e4202887c7f43e660b94
Chung <chung19941024@gmail.com>
2022-07-29 02:57:34 +0800

    fix receive message scroll

--- src/app/pages/chat-room/chat-room.component.ts
+++ src/app/pages/chat-room/chat-room.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { debounceTime, Subscription } from 'rxjs';
 import { CommonService } from 'src/app/services/common.service';
 
 @Component({
@@ -6,13 +14,35 @@ import { CommonService } from 'src/app/services/common.service';
   templateUrl: './chat-room.component.html',
   styleUrls: ['./chat-room.component.scss'],
 })
-export class ChatRoomComponent implements OnInit {
+export class ChatRoomComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('container', { static: false })
+  containerEle?: ElementRef<HTMLElement>;
   messages$ = this.commonService.onChatMessages();
   myName = '';
+
+  subs: Subscription[] = [];
 
   constructor(private commonService: CommonService) {}
 
   ngOnInit(): void {
     this.myName = localStorage.getItem('name') || '';
+  }
+
+  ngAfterViewInit(): void {
+    this.subs.push(
+      this.commonService
+        .onChatMessages()
+        .pipe(debounceTime(100))
+        .subscribe(() => {
+          if (this.containerEle?.nativeElement) {
+            this.containerEle.nativeElement.scrollTop =
+              this.containerEle?.nativeElement.scrollHeight;
+          }
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach((sub) => sub.unsubscribe());
   }
 }
